7ae2818911046de8714a50041faaa964b206444c
Florian Chague <florian.chague2@gmail.com>
2026-01-02 06:43:00 +0100

    modificaiton des vite_server_url dans les .env

--- gantt/backend/src/server.js
+++ gantt/backend/src/server.js
@@ -3,4 +3,4 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const PORT = Number(process.env.PORT || 3000);
-app.listen(PORT, () => console.log(`✅ Backend running on http://localhost:${PORT}`));
+//app.listen(PORT, () => console.log(`✅ Backend running on http://localhost:${PORT}`));
